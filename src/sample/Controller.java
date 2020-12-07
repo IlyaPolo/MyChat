e4902ab833d377d3c2c6d651c64eeb875577b6f2
ilya.poloshovets <ilya.poloshovets@gmail.com>
2020-12-07 12:12:56 +0300

    Задание к 4 уроку

---
+++ src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+
+    public void sendSomething(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
